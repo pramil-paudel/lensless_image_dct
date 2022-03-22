218bc19368db3c362837662bb9df94ca723d120f
pramil-paudel <ppaudel35@gmail.com>
2022-03-22 16:52:17 -0500

    sciprt added

---
+++ train_rec_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p intel
+#SBATCH -N 5
+#SBATCH -n 10
+#SBATCH -c 1
+#SBATCH --mem=1GB
+#SBATCH -t 02:00:00 
+#SBATCH -J training_rec_images
+#SBATCH -o slurm-%j.out
+ 
+python model1/main.py
+#python LearnToPayAttention_image_rec/train.py --attn_mode after --outf logs_after --normalize_attn --log_images
